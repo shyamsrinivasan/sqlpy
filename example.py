9013a05cf52e0350dbdf2aa5fcc3137fcc8d6be8
Shyam Srinivasan <shyamsweth@gmail.com>
2021-10-20 14:50:56 +0530

    example to insert rows into db tables

--- example.py
+++ example.py
@@ -10,35 +10,16 @@ dbconfig = {'user': 'root',
 
 try:
     cnx = mysql.connector.connect(**dbconfig)
-    # cnx = mysql.connector.connect(user='root', password='root',
-    #                               host='127.0.0.1',database='testDB')
     cursor = cnx.cursor()
-    cus_id = cursor.lastrowid
-    print(cus_id)
-    print('\n')
 
-    # data format to be entered
-    add_client = ("INSERT INTO taxdata.clients (clientid, firstname, lastname, pan) "
-                  "VALUES (%(client_id)s, %(first_name)s, %(last_name)s, %(pan)s)")
-    # add_address = ("INSERT INTO taxdata.address"
-    #                "(clientid, streetnumber, housenum, streetname, locality, city, state, pin) "
-    #                "VALUES (%(client_id)d, %(street_num)s, %(house_num)s, %(street_name)s, %(locale)s, %(city)s,"
-    #                "%(state)s, %(pin)s)")
-    # add_identity = ("INSERT INTO taxdata.identity "
-    #                 "(pan, clientid, portalpass)"
-    #                 "VALUES (%(pan)s, %(client_id)s, %(portal_pass)s)")
+    query = "SELECT firstname, lastname FROM taxdata.clients WHERE clientid=%(client_id)s;"
 
-    data_client = {'client_id': int(20003), 'first_name': 'Jane', 'last_name': 'Doe', 'pan': 'AXYD1234ST'}
+    detail = {'client_id': 20002}
+    # Search SQL db
+    cursor.execute(query, detail)
 
-    # Insert new client
-    cursor.execute(add_client, data_client)
-    client_id = cursor.lastrowid
-
-    print(client_id)
-    print('\n')
-
-    # making sure data is commited to db
-    cnx.commit()
+    for (first_name, last_name) in cursor:
+        print("{} {} is a client".format(first_name, last_name))
 
     cursor.close()
     cnx.close()
@@ -52,10 +33,7 @@ except mysql.connector.Error as err:
         print(err)
 else:
     cnx.close()
-# from mysql.connector import (connection)
-# cnx = connection.MySQLConnection(user='root', password='root',
-#                                  host='127.0.0.1', database='testDB')
-# cnx.close()
+
 
 
 
