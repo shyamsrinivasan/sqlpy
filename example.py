550a809b1a3057f0695ab0201f9c96afe404ef1b
Shyam Srinivasan <shyamsweth@gmail.com>
2022-01-13 00:37:39 +0530

    move connecting and quering db to separate fun

--- example.py
+++ example.py
@@ -1,46 +1,67 @@
 import mysql.connector
 from mysql.connector import errorcode
+from query import querydb
 
 
-dbconfig = {'user': 'root',
-            'password': 'root',
-            'host': '127.0.0.1',
-            'database': 'taxdata',
-            'raise_on_warnings': True}
+if __name__ == '__main__':
+    """script to test mySQL python connector"""
+    # step 1 - connect to sql db/query db for values
+    dbconfig = {'user': 'root',
+                'password': 'root',
+                'host': '127.0.0.1',
+                'database': 'taxdata',
+                'raise_on_warnings': True}
 
-# display all databases in the current SQL server
-showdb_query = "SHOW DATABASES"
+    # query = "SELECT firstname, lastname FROM taxdata.clients WHERE clientid=%(client_id)s;"
+    # detail = {'client_id': 20002}
+    query = "SELECT firstname, lastname, clientid FROM clients;"
+    querydb(dbconfig, query, printflag=True)
 
-try:
-    cnx = mysql.connector.connect(**dbconfig)
-    cursor = cnx.cursor()
+    # display all databases in the current SQL server
+    # showdb_query = "SHOW DATABASES"
 
-    query = "SELECT firstname, lastname FROM taxdata.clients WHERE clientid=%(client_id)s;"
+    # step 2 - connect to sql db/add values to db
 
-    detail = {'client_id': 20002}
-    # Search SQL db
-    cursor.execute(query, detail)
+    try:
+        cnx = mysql.connector.connect(**dbconfig)
+        cursor = cnx.cursor()
 
-    # show all db in SQL server
-    # cursor.execute(showdb_query)
-    # for db in cursor:
-    #     print(db)
+        # insert data into existing db table
+        # add_client = ("INSERT INTO clients "
+        #               "(clientid, firstname, lastname, pan) "
+        #               "VALUES (%(client_id)s, %(first_name)s, %(last_name)s, %(pan)s)")
+        # # add_identity = "INSERT INTO identity"
+        # # add_address = "INSERT INTO address"
+        # client_data = {'client_id': 20004, 'first_name': 'Asuma', 'last_name': 'Sarutobi', 'pan': 'SATC1248XY'}
 
-    for (first_name, last_name) in cursor:
-        print("{} {} is a client".format(first_name, last_name))
+        # mysql db query
+        # query = "SELECT firstname, lastname FROM taxdata.clients WHERE clientid=%(client_id)s;"
+        # detail = {'client_id': 20002}
+        query = "SELECT firstname, lastname, clientid FROM clients;"
 
-    cursor.close()
-    cnx.close()
+        # cursor.execute(add_client, client_data)
+        cursor.execute(query)
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Username or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+        # show all db in SQL server
+        # cursor.execute(showdb_query)
+        # for db in cursor:
+        #     print(db)
+
+        for (first_name, last_name, client_id) in cursor:
+            print("{} {} is a client with ID: {}".format(first_name, last_name, client_id))
+
+        cursor.close()
+        cnx.close()
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Username or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
     else:
-        print(err)
-else:
-    cnx.close()
+        cnx.close()
 
 
 
--- query.py
+++ query.py
@@ -2,34 +2,41 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
-dbconfig = {'user': 'root',
-            'password': 'root',
-            'host': '127.0.0.1',
-            'database': 'taxdata',
-            'raise_on_warnings': True}
+def querydb(dbconfig={}, query='', query_args={}, printflag=False):
+    """connect to mysql db using connector"""
 
-try:
-    cnx = mysql.connector.connect(**dbconfig)
-    cursor = cnx.cursor()
-
-    query = "SELECT firstname, lastname FROM taxdata.clients WHERE clientid=%(client_id)s;"
-
-    detail = {'client_id': 20002}
-    # Search SQL db
-    cursor.execute(query, detail)
-
-    for (first_name, last_name) in cursor:
-        print("{} {} is a client".format(first_name, last_name))
-
-    cursor.close()
-    cnx.close()
-
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Username or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+    flag = False
+    if not dbconfig:
+        print('Empty database configuration information')
+        return flag
     else:
-        print(err)
-else:
-    cnx.close()
+        try:
+            cnx = mysql.connector.connect(**dbconfig)
+            cursor = cnx.cursor()
+            if query_args:
+                cursor.execute(query, query_args)   # execute given query in mysql object
+            else:
+                cursor.execute(query)
+            if printflag:
+                # print statement only good for taxdb.clients
+                for (first_name, last_name, client_id) in cursor:
+                    print("{} {} is a client with ID: {}".format(first_name, last_name, client_id))
+            cursor.close()
+            cnx.close()
+
+            flag = True
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Username or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            cnx.close()
+
+    return flag
+
+
+
