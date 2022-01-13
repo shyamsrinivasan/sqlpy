95149ed0ec196130748570fb089060c9a90c1fd7
Shyam Srinivasan <shyamsweth@gmail.com>
2022-01-13 23:12:45 +0530

    change mutable input arg to None (immutable)

--- query.py
+++ query.py
@@ -2,28 +2,27 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
-def querydb(dbconfig={}, query='', query_args={}, printflag=False):
+def querydb(dbconfig=None, query='', query_args=None, printflag=False):
     """connect to mysql db using connector"""
 
     flag = False
-    if not dbconfig:
+    if dbconfig is None:
         print('Empty database configuration information')
         return flag
     else:
         try:
             cnx = mysql.connector.connect(**dbconfig)
             cursor = cnx.cursor()
-            if query_args:
-                cursor.execute(query, query_args)   # execute given query in mysql object
-            else:
+            if query_args is None:
                 cursor.execute(query)
+            else:
+                cursor.execute(query, query_args)  # execute given query in mysql object
             if printflag:
                 # print statement only good for taxdb.clients
                 for (first_name, last_name, client_id) in cursor:
                     print("{} {} is a client with ID: {}".format(first_name, last_name, client_id))
             # cursor.close()
             # cnx.close()
-
             flag = True
 
         except mysql.connector.Error as err:
