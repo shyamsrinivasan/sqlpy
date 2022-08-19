02e8b62dd599d7bb92eb2cd028e6ccdd16a98ffc
Shyam Srinivasan <shyamsweth@gmail.com>
2022-08-19 10:06:42 +0530

    add relationships for foreign keys

--- tables.py
+++ tables.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, TIMESTAMP
+from sqlalchemy import Float
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 
 class Customer(Base):
+    """class with customer details only"""
     __tablename__ = 'customer'
 
     id = Column(Integer, primary_key=True)
@@ -13,17 +15,50 @@ class Customer(Base):
     email = Column(String(30))
     phone = Column(Integer)
 
+    taxes = relationship("TaxInfo", back_populates="customer_info", cascade="all, delete")
+    charges = relationship("Transactions", back_populates="customer_info")
+
     def __repr__(self):
         return f"User(id={self.id!r}, firstname={self.firstname!r}, lastname={self.lastname!r})"
 
 
-class Identity(Base):
-    __tablename__ = 'identity'
+class TaxInfo(Base):
+    """class with all tax-related information on client"""
+    __tablename__ = 'tax_info'
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('customer.id'))
-    pan = Column(String(12), index=True)
+    pan = Column(String(12), index=True, nullable=False)
+    # add columns: portal_password, aadhaar
+
+    customer_info = relationship("Customer", back_populates="taxes")
 
     def __repr__(self):
         return f"Identity(id={self.id!r}, user_id={self.user_id!r}, pan={self.pan!r})"
-    
+
+
+class Transactions(Base):
+    """class with list of all transactions done for all customers"""
+
+    __tablename__ = 'transactions'
+
+    id = Column(Integer, primary_key=True)
+    date = Column(TIMESTAMP)
+    user_id = Column(Integer, ForeignKey('customer.id'))
+    transaction_type = Column(String(50), nullable=False)
+    cost = Column(Float)
+    # add columns: performed_by
+
+    customer_info = relationship("Customer", back_populates="charges")
+
+    def __repr__(self):
+        return f"Identity(id={self.id!r}, date = {self.date!r}, user_id={self.user_id!r}, " \
+               f"transaction_type = {self.transaction_type!r}, cost={self.cost!r})"
+
+
+def create_table(engine):
+    """create tables from declarative classes in DB defined in engine"""
+
+    Base.metadata.create_all(engine)
+
+
