4fb090e9b480026ee221a996e6ba631178a1fb6b
Shyam Srinivasan <shyamsweth@gmail.com>
2022-09-16 11:04:05 +0530

    get all added data as single df

--- alscript.py
+++ alscript.py
@@ -7,16 +7,16 @@ if __name__ == '__main__':
     ops_obj = Operations()
 
     # drop all tables
-    ops_obj.drop_table(engine)
+    # ops_obj.drop_table(engine)
     # create all tables in db
-    ops_obj.create_table(engine)
+    # ops_obj.create_table(engine)
 
     # reflect table from DB given a Dbcon instance
     ops_obj.reflect_table(engine)
 
     # add data from file/dictionary
     file_name = os.path.join(os.getcwd(), 'sampleinfo.xlsx')
-    customers, tax_info, address = ops_obj.add_data(session, file_name=file_name)
+    customers = ops_obj.add_data(session, file_name=file_name)
 
     # delete data from db
     flag_2 = ops_obj.delete_data(session, table_name='tax_info', column='user_id',
