a54646d97b7ce97c78e2b7f12af12cf24749e27f
Shyam Srinivasan <shyamsweth@gmail.com>
2022-09-24 23:19:32 +0530

    add contact form and hidden field to pass next value

--- taxapp/admin/forms.py
+++ taxapp/admin/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import HiddenField, EmailField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
@@ -8,5 +9,17 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', [DataRequired(message='Please enter a username')])
     password = PasswordField('Password', [DataRequired(message='Please enter a password')])
+    next = HiddenField('Next Value')
 
     submit = SubmitField('Sign in')
+
+
+class ContactForm(FlaskForm):
+    """Form for contacting creator"""
+
+    name = StringField('Name', [DataRequired()])
+    email = EmailField('Email', [Email(message='Not a valid email address'), DataRequired()])
+    message = TextAreaField('Message', [DataRequired(), Length(min=4, message='Your message is too short')])
+
+    recaptcha = RecaptchaField()
+    submit = SubmitField('Send Message')
