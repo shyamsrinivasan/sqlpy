6b5484c7a0344825c839448bb388caf98f3b12d5
Shyam Srinivasan <shyamsweth@gmail.com>
2022-09-28 23:56:05 +0530

    add customer table class

--- taxapp/admin/models.py
+++ taxapp/admin/models.py
@@ -47,3 +47,27 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Customer(UserMixin, db.Model):
+    __tablename__ = 'test_customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    fullname = db.Column(db.String(40))
+    email = db.Column(db.String(30))
+    phone = db.Column(db.String(10))
+    type = db.Column(db.Enum('personal', 'commercial', name='customer_type'), nullable=False)
+    date_added = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
+    date_updated = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
+    # added_user
+    # updated_user
+
+    def set_full_name(self):
+        """set value of fullname column using first and last name"""
+        self.fullname = self.firstname + ' ' + self.lastname
+
+    def __repr__(self):
+        return f"Customer(id={self.id!r}, name={self.fullname!r}, email={self.email!r}, " \
+               f"phone={self.phone!r}, type={self.type!r}, added_on={self.date_added!r})"
