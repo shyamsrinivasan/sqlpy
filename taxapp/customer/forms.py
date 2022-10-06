98c07c8bef373d91e512ee5c10510056ad98c9f7
Shyam Srinivasan <shyamsweth@gmail.com>
2022-10-06 12:48:24 +0530

    add new customer form class

---
+++ taxapp/customer/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, DateField, SelectField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired, Email, Optional
+from wtforms.validators import ValidationError
+
+
+def phone_num(minimum=-1, maximum=-1):
+    message = 'Must be between %d (without +country code) and %d (with +country code) characters long' % (minimum, maximum)
+
+    def _phone_num(form, field):
+        l = field.data and len(field.data) or 0
+        if l < minimum or maximum != -1 and l > maximum:
+            raise ValidationError(message)
+    return _phone_num
+
+
+class CustomerSignup(FlaskForm):
+    """form to add customer to database"""
+
+    first_name = StringField('First Name', [DataRequired(message='Please provide a first name')])
+    last_name = StringField('Last Name', [DataRequired(message='Please provide a last name')])
+    email = EmailField('Email', [Email(message='Not a valid email address'), Optional()])
+    phone = StringField('Phone', [Optional(), phone_num(minimum=10, maximum=14)])
+    dob = DateField('Date of Birth', [DataRequired()])
+    customer_type = SelectField('Employee Type', [DataRequired()], choices=[('Personal', 'personal'),
+                                                                            ('Commercial', 'business')])
+    # recaptcha = RecaptchaField()
+    submit = SubmitField('Add Customer')
--- taxapp/customer/views.py
+++ taxapp/customer/views.py
@@ -1,13 +1,15 @@
 from flask import render_template
 from . import customer_bp
+from .forms import CustomerSignup
 from flask_login import login_required
 
 
-@customer_bp.route('/add_customers')
+@customer_bp.route('/add_customers', methods=['GET', 'POST'])
 # @login_required
 def add_customer():
     """route to access customer addition form/page"""
-    return render_template('/add.html')
+    form = CustomerSignup()
+    return render_template('/add.html', form=form)
 
 
 @customer_bp.route('/remove_customer')
