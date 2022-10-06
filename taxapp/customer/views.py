56b98db0ee99a4452abb8c454a5df9958f47e27d
Shyam Srinivasan <shyamsweth@gmail.com>
2022-10-06 15:12:26 +0530

    change view func names

--- taxapp/customer/views.py
+++ taxapp/customer/views.py
@@ -4,24 +4,24 @@ from .forms import CustomerSignup
 from flask_login import login_required
 
 
-@customer_bp.route('/add_customers', methods=['GET', 'POST'])
+@customer_bp.route('/customer/add', methods=['GET', 'POST'])
 # @login_required
-def add_customer():
+def add():
     """route to access customer addition form/page"""
     form = CustomerSignup()
     return render_template('/add.html', form=form)
 
 
-@customer_bp.route('/remove_customer')
+@customer_bp.route('/customer/remove')
 # @login_required
-def remove_customer():
+def remove():
     """route access customer removal form/page"""
     return render_template('/remove.html')
 
 
-@customer_bp.route('/modify_customer')
+@customer_bp.route('/customer/modify')
 # @login_required
-def modify_customer():
+def modify():
     """route to change customer details"""
     return render_template('/modify.html')
 
