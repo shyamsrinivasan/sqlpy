c210dda0aab1b51eaf984a2fb7f9036230a28413
Shyam Srinivasan <shyamsweth@gmail.com>
2022-10-08 00:35:19 +0530

    add func to add customer to db

--- taxapp/customer/views.py
+++ taxapp/customer/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, flash
 from . import customer_bp
 from .forms import CustomerSignup
 from flask_login import login_required
@@ -10,6 +10,9 @@ def add():
     """route to access customer addition form/page"""
     form = CustomerSignup()
     if form.validate_on_submit():
+        _add_customer(request.form)
+        customer_name = 'customer name'
+        flash('Addition of new customer {} successful'.format(customer_name))
         return 'Customer added successfully'
     return render_template('/add.html', form=form)
 
@@ -27,5 +30,18 @@ def modify():
     """route to change customer details"""
     return render_template('/modify.html')
 
+
+def _add_customer(form_obj):
+    """take user details in form_obj to create Customer object and
+    add as row to customer table"""
+    # generate user object
+    # new_user_obj = User(firstname=form_obj['first_name'], lastname=form_obj['last_name'],
+    #                     email=form_obj['email'], phone=form_obj['phone'],
+    #                     username=form_obj['username'])
+    # # add hashed password to db
+    # new_user_obj.set_password(form_obj['password'])
+    # db.session.add(new_user_obj)
+    # db.session.commit()
+    return None
 
 
