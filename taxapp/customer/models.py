916950360c08d2da708be0b9be5b2b382ff616cf
Shyam Srinivasan <shyamsweth@gmail.com>
2022-10-08 14:29:56 +0530

    pdated customer table from flaskap

--- taxapp/customer/models.py
+++ taxapp/customer/models.py
@@ -9,14 +9,14 @@ class Customer(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
-    fullname = db.Column(db.String(40))
-    email = db.Column(db.String(30))
+    fullname = db.Column(db.String(40), index=True)
+    email = db.Column(db.String(30), index=True)
     phone = db.Column(db.String(10))
     type = db.Column(db.Enum('personal', 'commercial', name='customer_type'), nullable=False)
     date_added = db.Column(db.DateTime(timezone=True), nullable=False, server_default=db.func.now())
     date_updated = db.Column(db.DateTime(timezone=True), onupdate=db.func.now())
-    # added_user
-    # updated_user
+    added_user = db.Column(db.String(20))
+    updated_user = db.Column(db.String(20))
 
     def set_full_name(self):
         """set value of fullname column using first and last name"""
@@ -25,6 +25,13 @@ class Customer(UserMixin, db.Model):
     def set_full_phone(self, country_code, phone_number):
         """set phone number with country code"""
         self.phone = country_code + phone_number
+
+    def set_added_user(self, change_type, username):
+        """set added_user and updated_user"""
+        if change_type == 'add':
+            self.added_user = username
+        elif change_type == 'update':
+            self.updated_user = username
 
     def __repr__(self):
         return f"Customer(id={self.id!r}, name={self.fullname!r}, email={self.email!r}, " \
