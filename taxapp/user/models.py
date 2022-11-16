f0840c3b1eba0caf91350daee81eaaf63d8e6352
Shyam Srinivasan <shyamsweth@gmail.com>
2022-11-16 22:42:59 +0530

    seperate userlog table and class - not impleted
    user log table is currently unused in app/routes

--- taxapp/user/models.py
+++ taxapp/user/models.py
@@ -70,3 +70,11 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class UserLog(db.Model):
+    __tablename__ = 'test_userlog'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True)
+    login_time = db.Column(db.DateTime(timezone=True))
